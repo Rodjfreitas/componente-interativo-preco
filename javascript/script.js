20d7974cd0bfbe17c4c11d3c8ee44094beb6b786
Rodjfreitas <rodrigofreitas2011@live.com>
2023-02-08 11:06:58 -0300

    feat: ball transition code

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+function themeyearly (){
+  let ball = document.querySelector('.ball')
+  ball.style.transform = "translateX(30px)"
+}
+
+function thememonthly(){
+  let ball = document.querySelector('.ball')
+  ball.style.transform = "translateX(0px)"
+}

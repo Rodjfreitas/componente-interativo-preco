1e28c85b75224b06efc470d4cb38c7d9df8a9ac8
Rodjfreitas <rodrigofreitas2011@live.com>
2023-02-10 15:46:59 -0300

    feat: animation transition period select

--- javascript/script.js
+++ javascript/script.js
@@ -7,11 +7,24 @@ preco.innerText = bar.value
 function themeyearly (){
   let ball = document.querySelector('.ball')
   ball.style.transform = "translateX(30px)"
+  let discount = document.querySelector('.discount')
+  discount.style.border = '1px solid red'
+  discount.style.borderRadius = '15px'
+  discount.style.color = 'red'
+  discount.style.textDecoration = 'line-through'
+
+  
 }
 
 function thememonthly(){
   let ball = document.querySelector('.ball')
   ball.style.transform = "translateX(0px)"
+  
+  let discount = document.querySelector('.discount')
+  discount.style.border = 'none'
+  discount.style.borderRadius = 'none'
+  discount.style.color = 'black'
+  discount.style.textDecoration = 'none'
 }
 
 function changePrice(){
